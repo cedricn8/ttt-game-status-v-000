0931dc55487d532f674673dfb19a990447cb876b
Cedric Niguidula <cedcn-9mail@yahoo.com>
2018-03-30 06:55:17 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -50,7 +50,7 @@ def full?(board)
 end
 
 def draw?(board)
-  if (!won?(board)) && full?(board))
+  if (!won?(board)) && full?(board)
     return true
   elsif (!won?(board) && !full?(board)) || won?(board)
     return false
